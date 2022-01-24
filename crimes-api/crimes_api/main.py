1d067a2ccfe50d9a18445110a1d678a018a64d23
ShreyaTalati <talatishreya2@gmail.com>
2022-01-24 22:59:25 +0530

    Configured mongodb and kafka connect

--- crimes-api/crimes_api/main.py
+++ crimes-api/crimes_api/main.py
@@ -8,6 +8,7 @@ from kafka import KafkaProducer, KafkaConsumer
 from fastapi import FastAPI
 from pydantic import BaseModel
 from pymongo import MongoClient
+import requests
 
 
 class Crime(BaseModel):
@@ -36,8 +37,35 @@ consumer = KafkaConsumer(
 try:
     client = MongoClient('mongo-db', 27017)
     db = client.crime_info
+    db2 = client.crime_info_test
+    collection_name = db["raw_crime_info"]
+    collection_name_test = db2["raw_crime_info_test"]
 except Exception as e:
     print("Could not connect to MongoDB: {}".format(e))
+
+try:
+    # collection_name = db["raw_crime_info"]
+    url = "http://localhost:8083/connectors"
+
+    payload = {"name": "mongo-sink2",
+               "config": {"connector.class":"com.mongodb.kafka.connect.MongoSinkConnector",
+                          "tasks.max":"1",
+                          "topics":"crime",
+                          "connection.uri":"mongodb://mongo-db:27017",
+                          "database":"crime_info_test",
+                          "collection":"raw_crime_info_test",
+                          "key.converter": "org.apache.kafka.connect.storage.StringConverter",
+                          "value.converter": "org.apache.kafka.connect.json.JsonConverter",
+                          "value.converter.schemas.enable": "false"
+                         }
+               }
+    headers = {
+      'Content-Type': 'application/json'
+    }
+
+    response = requests.request("POST", url, headers=headers, data=payload)
+except Exception as e:
+    print("Could not post request to sink {}".format(e))
 
 
 @app.post("/crimes")
@@ -46,31 +74,39 @@ def report_crime(crime: Crime):
     crime = {**crime.dict(), "timestamp": time.time(), "id": str(uuid.uuid4())}
     producer.send("crime", crime)
     producer.flush()
-    return {"id": crime["id"]}
-
-
-@app.get("/crimes")
-def get_crimes():
     crimes = []
     inserted = []
     for msg in consumer:
         crimes.append(msg.value)
-        record = msg.value
-        crime_id = record['id']
-        email_id = record['email_id']
-        lat = record['lat']
-        long = record['lon']
-        type = record['type']
-        timestamp = record['timestamp']
-        status = "open"
-        crimes.append(msg.value)
-        try:
-            crime_rec = {'crime_id': crime_id, 'email_id': email_id, 'lat': lat,
-                         'long': long, 'type': type, 'timestamp': timestamp, 'status': status}
-            inserted.append(json.dumps(crime_rec))
-            collection_name = db["raw_crime_info"]
-            collection_name.insert_one(crime_rec)
-        except Exception as e:
-            # pass
-            print("Could not insert into MongoDB: {}".format(e))
-    return ({"crimes": crimes},{"inserted": inserted})
+    #     record = msg.value
+    #     crime_id = record['id']
+    #     email_id = record['email_id']
+    #     lat = record['lat']
+    #     long = record['lon']
+    #     type = record['type']
+    #     timestamp = record['timestamp']
+    #     status = "open"
+    #     crimes.append(msg.value)
+    #     try:
+    #         crime_rec = {'crime_id': crime_id, 'email_id': email_id, 'lat': lat,
+    #                      'long': long, 'type': type, 'timestamp': timestamp, 'status': status}
+    #         inserted.append(json.dumps(crime_rec))
+    #         collection_name.insert_one(crime_rec)
+    #     except Exception as e:
+    #         # pass
+    #         print("Could not insert into MongoDB: {}".format(e))
+    return ({"id": crime["id"]}, {"crimes": crimes}, {"inserted": inserted})
+
+
+@app.get("/crimes")
+def get_crimes():
+    # crimes = []
+    col = db["raw_crime_info"]
+    x = col.find({"_id": 0})
+    return x
+
+
+@app.get("/crimes/{crime_id}")
+def get_crime(crime_id):
+    # col = db["raw_crime_info"]
+    return (collection_name.find_one({"crime_id": crime_id}, {"_id": 0}), db.list_collection_names())
